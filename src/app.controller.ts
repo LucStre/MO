4f231130129f12e6bcfdd01f6e4b0d6fd6060528
Lucija <lucija.strejcek@fer.hr>
2022-05-16 14:13:39 +0200

    Add user service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,72 @@
-import { Get, Controller, Render } from '@nestjs/common';
+import {
+  Get,
+  Controller,
+  Param,
+  Delete,
+  Put,
+  Post,
+  Body,
+} from '@nestjs/common';
+import { UserService } from './services/user.service';
+import { korisnik as UserModel } from '@prisma/client';
 
 @Controller()
 export class AppController {
-  @Get()
-  @Render('index')
-  root() {
-    return { message: 'Hello world!' };
+  constructor(private readonly userService: UserService) {}
+
+  @Get('user/:id')
+  async getUserById(@Param('id') id: string): Promise<UserModel> {
+    return this.userService.user({ id: Number(id) });
+  }
+
+  @Get('users')
+  async getUsers(): Promise<UserModel[]> {
+    return this.userService.users({});
+  }
+
+  @Post('user')
+  async createUser(
+    @Body()
+    userData: {
+      ime: string;
+      prezime: string;
+      brojtelefona: string;
+      email: string;
+    },
+  ): Promise<UserModel> {
+    const { ime, prezime, brojtelefona, email } = userData;
+    return this.userService.createUser({
+      ime,
+      prezime,
+      brojtelefona,
+      email,
+    });
+  }
+
+  @Put('user/:id')
+  async updateUser(
+    @Param('id') id: string,
+    @Body()
+    userData: {
+      ime?: string;
+      prezime?: string;
+      brojtelefona?: string;
+      email?: string;
+    },
+  ): Promise<UserModel> {
+    return this.userService.updateUser({
+      where: { id: Number(id) },
+      data: {
+        ime: userData.ime,
+        prezime: userData.prezime,
+        brojtelefona: userData.brojtelefona,
+        email: userData.email,
+      },
+    });
+  }
+
+  @Delete('user/:id')
+  async deletePost(@Param('id') id: string): Promise<UserModel> {
+    return this.userService.deleteUser({ id: Number(id) });
   }
 }
---
+++ src/services/user.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+import { korisnik, Prisma } from '@prisma/client';
+
+@Injectable()
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  async user(
+    korisnikWhereUniqueInput: Prisma.korisnikWhereUniqueInput,
+  ): Promise<korisnik | null> {
+    return this.prisma.korisnik.findUnique({
+      where: korisnikWhereUniqueInput,
+    });
+  }
+
+  async users(params: {
+    skip?: number;
+    take?: number;
+    cursor?: Prisma.korisnikWhereUniqueInput;
+    where?: Prisma.korisnikWhereInput;
+    orderBy?: Prisma.korisnikOrderByWithRelationInput;
+  }): Promise<korisnik[]> {
+    const { skip, take, cursor, where, orderBy } = params;
+    return this.prisma.korisnik.findMany({
+      skip,
+      take,
+      cursor,
+      where,
+      orderBy,
+    });
+  }
+
+  async createUser(data: Prisma.korisnikCreateInput): Promise<korisnik> {
+    return this.prisma.korisnik.create({
+      data,
+    });
+  }
+
+  async updateUser(params: {
+    where: Prisma.korisnikWhereUniqueInput;
+    data: Prisma.korisnikUpdateInput;
+  }): Promise<korisnik> {
+    const { where, data } = params;
+    return this.prisma.korisnik.update({
+      data,
+      where,
+    });
+  }
+
+  async deleteUser(where: Prisma.korisnikWhereUniqueInput): Promise<korisnik> {
+    return this.prisma.korisnik.delete({
+      where,
+    });
+  }
+}
